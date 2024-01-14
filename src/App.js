38291e296e38197ae7bcf2a7a76ab2ee9ccf61f8
cityfarm <cityfarm19@gmail.com>
2024-01-14 22:28:26 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Phương";
+  const age = "45";
+  const isMale = true;
+  const student = {
+    name: "Phan Phuong"
+  };
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Phương đẹp trai!
+          Hi {name} - {age} - {isMale ? 'male' : 'female'}
         </p>
+
+        <p>
+          student.name = {student.name}
+        </p>
+
+        <ul class="nav justify-content-center">
+          {
+            colorList.map(color => (
+              <li key={color} style={{ color }}>{color}</li>
+            ))
+          }
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
