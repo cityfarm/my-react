bbbc55019ce33a34172adea0c623e7318b087907
cityfarm <cityfarm19@gmail.com>
2024-01-15 16:27:39 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name = "Phương";
-  const age = "45";
-  const isMale = true;
-  const student = {
-    name: "Sinh vien Phuong"
-  };
-  const colorList = ['red', 'green', 'blue'];
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {name} - {age} - {isMale ? 'male' : 'female'}
-        </p>
-
-        <p>
-          student.name = {student.name}
-        </p>
-
-        <ul class="nav justify-content-center">
-          {
-            colorList.map(color => (
-              <li key={color} style={{ color }}>{color}</li>
-            ))
-          }
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Học React
-        </a>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
